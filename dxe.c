bdc941559d8e885f02c1d979f6f4b78f7bb11522
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-06-12 15:24:21 +0200

    Do not encrypt null frame
    Null frame must not be encrypted.
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- dxe.c
+++ dxe.c
@@ -576,7 +576,9 @@ int wcn36xx_dxe_tx(struct wcn36xx *wcn,
 	}
 
 	wcn36xx_prepare_tx_bd(ctl->bd_cpu_addr, skb->len, header_len);
-	if (!is_high && WCN36XX_BSS_KEY == wcn->en_state) {
+	/* Do not encrypt NULL and MGMT frames */
+	if (!is_high && WCN36XX_BSS_KEY == wcn->en_state &&
+	    !ieee80211_is_nullfunc(hdr->frame_control)) {
 		wcn36xx_dbg(WCN36XX_DBG_DXE, "DXE Encription enabled");
 		wcn36xx_fill_tx_bd(wcn, ctl->bd_cpu_addr, broadcast, 0, hdr,
 				   tx_ack);
