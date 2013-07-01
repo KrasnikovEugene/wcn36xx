KrasnikovEugene/wcn36xx

commit a0e6bc39479c78e7ed3dbd2a384bde52d5be65b0

Cleanup includes
Removing unused includes

Signed-off-by: Olof Johansson <dev@skyshaper.net>

dxe.c
@@ -19,11 +19,9 @@
  * through low channels data packets are transfered
  * through high channels managment packets are transfered
  */
-#include <linux/interrupt.h>
-#include <linux/ieee80211.h>
-#include "dxe.h"
-#include "txrx.h"
+
 #include "wcn36xx.h"
+#include "txrx.h"
 
 static void wcn36xx_dxe_write_register(struct wcn36xx *wcn, int addr, int data)
 {

main.c
@@ -15,13 +15,8 @@
  */
 
 #include <linux/module.h>
-#include <linux/qcomwlan_pwrif.h>
-#include <linux/platform_device.h>
 #include <linux/wcnss_wlan.h>
-#include <linux/etherdevice.h>
 #include "wcn36xx.h"
-#include "dxe.h"
-#include "hal.h"
 
 unsigned int debug_mask;
 module_param(debug_mask, uint, 0644);

smd.c
@@ -16,8 +16,6 @@
 
 #include "smd.h"
 
-#include <linux/etherdevice.h>
-
 static int wcn36xx_smd_send_and_wait(struct wcn36xx *wcn, size_t len)
 {
 	int avail;

dxe.h
@@ -17,7 +17,6 @@
 #ifndef _DXE_H_
 #define _DXE_H_
 
-#include <linux/dma-mapping.h>
 /*
 TX_LOW	= DMA0
 TX_HIGH	= DMA4

smd.h
@@ -17,9 +17,7 @@
 #ifndef _SMD_H_
 #define _SMD_H_
 
-#include <mach/msm_smd.h>
 #include "wcn36xx.h"
-#include "hal.h"
 
 /* Max shared size is 4k but we take less.*/
 #define WCN36XX_NV_FRAGMENT_SIZE			3072

txrx.c
@@ -15,7 +15,6 @@
  */
 
 #include "txrx.h"
-#include <linux/ieee80211.h>
 
 #define RSSI0(x) (100 - ((x->phy_stat0 >> 24) & 0xff))
 

txrx.h
@@ -16,9 +16,8 @@
 
 #ifndef _TXRX_H_
 #define _TXRX_H_
+
 #include "wcn36xx.h"
-#include "smd.h"
-#include <linux/ieee80211.h>
 
 /* TODO describe all properties */
 #define WCN36XX_802_11_HEADER_LEN	24

wcn36xx.h
@@ -25,9 +25,9 @@
 #include <mach/msm_smd.h>
 #include <net/mac80211.h>
 
+#include "hal.h"
 #include "smd.h"
 #include "dxe.h"
-#include "hal.h"
 
 #define DRIVER_PREFIX "wcn36xx: "
 #define WLAN_NV_FILE               "wlan/prima/WCNSS_qcom_wlan_nv.bin"

commit 6f336e94162cd898f48c27a564a20bfe79fd7f46

Fix build after cleanup
Patch a0e6bc39479c78e7ed3dbd2a384bde52d5be65b0 broke
build.

Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

dxe.c
@@ -20,6 +20,7 @@
  * through high channels managment packets are transfered
  */
 
+#include <linux/interrupt.h>
 #include "wcn36xx.h"
 #include "txrx.h"
 

main.c
@@ -14,6 +14,7 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <linux/etherdevice.h>
 #include <linux/module.h>
 #include <linux/wcnss_wlan.h>
 #include "wcn36xx.h"

smd.c
@@ -14,6 +14,7 @@
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <linux/etherdevice.h>
 #include "smd.h"
 
 static int wcn36xx_smd_send_and_wait(struct wcn36xx *wcn, size_t len)