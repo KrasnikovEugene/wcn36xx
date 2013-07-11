6c8906127bb6f25b9b95236ff4efd65eb9c93b84
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-07-11 18:50:23 +0200

    Pass vif instead of iftype
    config_bss/sta will need other properties from vif.
    So let's pass vif pointer instead of iftype.
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- main.c
+++ main.c
@@ -385,7 +385,7 @@ static int wcn36xx_set_key(struct ieee80211_hw *hw, enum set_key_cmd cmd,
 			sta_priv->is_data_encrypted = true;
 			/* Reconfigure bss with encrypt_type */
 			wcn36xx_smd_config_bss(wcn,
-					       NL80211_IFTYPE_STATION,
+					       vif,
 					       sta->addr,
 					       true,
 					       wcn->beacon_interval);
@@ -523,7 +523,7 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 			wcn->is_joining = true;
 			wcn36xx_smd_join(wcn, bss_conf->bssid,
 					 vif->addr, WCN36XX_HW_CHANNEL(wcn));
-			wcn36xx_smd_config_bss(wcn, NL80211_IFTYPE_STATION,
+			wcn36xx_smd_config_bss(wcn, vif,
 					       bss_conf->bssid, false,
 					       wcn->beacon_interval);
 		} else {
@@ -564,10 +564,11 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 			wcn36xx_smd_set_link_st(wcn, bss_conf->bssid,
 						vif->addr,
 						WCN36XX_HAL_LINK_POSTASSOC_STATE);
-			wcn36xx_smd_config_bss(wcn, NL80211_IFTYPE_STATION,
+			wcn36xx_smd_config_bss(wcn, vif,
 					       bss_conf->bssid,
 					       true, wcn->beacon_interval);
-			wcn36xx_smd_config_sta(wcn, bss_conf->bssid, vif->addr, vif->type);
+			wcn36xx_smd_config_sta(wcn, vif, bss_conf->bssid,
+					       vif->addr);
 
 		} else {
 			wcn36xx_dbg(WCN36XX_DBG_MAC,
@@ -602,7 +603,7 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 
 		if (bss_conf->enable_beacon) {
 			wcn->beacon_enable = true;
-			wcn36xx_smd_config_bss(wcn, vif->type,
+			wcn36xx_smd_config_bss(wcn, vif,
 					       wcn->addresses[0].addr, false,
 					       wcn->beacon_interval);
 			skb = ieee80211_beacon_get_tim(hw, vif, &tim_off,
@@ -685,7 +686,7 @@ static int wcn36xx_sta_add(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
 	    vif->type == NL80211_IFTYPE_AP ||
 	    vif->type == NL80211_IFTYPE_MESH_POINT) {
 		wcn->aid = sta->aid;
-		wcn36xx_smd_config_sta(wcn, vif->addr, sta->addr, vif->type);
+		wcn36xx_smd_config_sta(wcn, vif, vif->addr, sta->addr);
 	}
 	return 0;
 }
