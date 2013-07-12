312e2b5bc42a3df74dd05c373aee422875025690
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-07-12 16:32:20 +0200

    Set HT caps in config_sta
    Configure HW with correct high throughput capabilities in config_sta.
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- smd.h
+++ smd.h
@@ -66,7 +66,8 @@ int wcn36xx_smd_config_bss(struct wcn36xx *wcn, struct ieee80211_vif *vif,
 			   const u8 *bssid, bool update, u16 beacon_interval);
 int wcn36xx_smd_delete_bss(struct wcn36xx *wcn);
 int wcn36xx_smd_config_sta(struct wcn36xx *wcn, struct ieee80211_vif *vif,
-			   const u8 *bssid, const u8 *sta_mac);
+			   struct ieee80211_sta *sta, const u8 *bssid,
+			   const u8 *sta_mac);
 int wcn36xx_smd_send_beacon(struct wcn36xx *wcn, struct sk_buff *skb_beacon,
 			    u16 tim_off, u16 p2p_off);
 int wcn36xx_smd_switch_channel(struct wcn36xx *wcn, int ch);
