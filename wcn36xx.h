49daf49d3561566a650e997216f19cfca531872d
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-07-17 15:41:08 +0200

    Set short_preamble_supported
    Check if HW supports short preamble or not.
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- wcn36xx.h
+++ wcn36xx.h
@@ -83,6 +83,7 @@ enum wcn36xx_debug_mask {
 #define WCN36XX_BAND(__wcn) (__wcn->hw->conf.chandef.chan->band)
 #define WCN36XX_CENTER_FREQ(__wcn) (__wcn->hw->conf.chandef.chan->center_freq)
 #define WCN36XX_LISTEN_INTERVAL(__wcn) (__wcn->hw->conf.listen_interval)
+#define WCN36XX_FLAGS(__wcn) (__wcn->hw->flags)
 
 static inline void buff_to_be(u32 *buf, size_t len)
 {
