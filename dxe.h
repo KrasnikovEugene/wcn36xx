75c7579d939301dcb5dbba35a5d73d3208f91a04
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-07-29 19:49:35 +0200

    Fix checkpatch warnings in DXE and HAL
    All errors and warnings after running checkpatch were fixed
    in following files:
    dxe.c
    dxe.h
    hal.h
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- dxe.h
+++ dxe.h
@@ -97,38 +97,71 @@ H2H_TEST_RX_TX = DMA2
 #define WCN36XX_DXE_TX_HIGH_OFFSET		0x500
 #define WCN36XX_DXE_RX_LOW_OFFSET		0x440
 #define WCN36XX_DXE_RX_HIGH_OFFSET		0x4C0
+
 /* Address of the next DXE descriptor */
 #define WCN36XX_DXE_CH_NEXT_DESC_ADDR		0x001C
-#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_TX_L	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_LOW_OFFSET + WCN36XX_DXE_CH_NEXT_DESC_ADDR)
-#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_TX_H	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_HIGH_OFFSET + WCN36XX_DXE_CH_NEXT_DESC_ADDR)
-#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_RX_L	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_LOW_OFFSET + WCN36XX_DXE_CH_NEXT_DESC_ADDR)
-#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_RX_H	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_HIGH_OFFSET + WCN36XX_DXE_CH_NEXT_DESC_ADDR)
+#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_TX_L	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_NEXT_DESC_ADDR)
+#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_TX_H	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_NEXT_DESC_ADDR)
+#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_RX_L	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_NEXT_DESC_ADDR)
+#define WCN36XX_DXE_CH_NEXT_DESC_ADDR_RX_H	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_NEXT_DESC_ADDR)
 
 /* DXE Descriptor source address */
 #define WCN36XX_DXE_CH_SRC_ADDR			0x000C
-#define WCN36XX_DXE_CH_SRC_ADDR_RX_L		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_LOW_OFFSET + WCN36XX_DXE_CH_SRC_ADDR)
-#define WCN36XX_DXE_CH_SRC_ADDR_RX_H		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_HIGH_OFFSET + WCN36XX_DXE_CH_SRC_ADDR)
+#define WCN36XX_DXE_CH_SRC_ADDR_RX_L		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_SRC_ADDR)
+#define WCN36XX_DXE_CH_SRC_ADDR_RX_H		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_SRC_ADDR)
 
 /* DXE Descriptor address destination address */
 #define WCN36XX_DXE_CH_DEST_ADDR		0x0014
-#define WCN36XX_DXE_CH_DEST_ADDR_TX_L		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_LOW_OFFSET + WCN36XX_DXE_CH_DEST_ADDR)
-#define WCN36XX_DXE_CH_DEST_ADDR_TX_H		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_HIGH_OFFSET + WCN36XX_DXE_CH_DEST_ADDR)
-#define WCN36XX_DXE_CH_DEST_ADDR_RX_L		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_LOW_OFFSET + WCN36XX_DXE_CH_DEST_ADDR)
-#define WCN36XX_DXE_CH_DEST_ADDR_RX_H		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_HIGH_OFFSET + WCN36XX_DXE_CH_DEST_ADDR)
+#define WCN36XX_DXE_CH_DEST_ADDR_TX_L		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_DEST_ADDR)
+#define WCN36XX_DXE_CH_DEST_ADDR_TX_H		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_DEST_ADDR)
+#define WCN36XX_DXE_CH_DEST_ADDR_RX_L		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_DEST_ADDR)
+#define WCN36XX_DXE_CH_DEST_ADDR_RX_H		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_DEST_ADDR)
 
 /* Interrupt status */
 #define WCN36XX_DXE_CH_STATUS_REG_ADDR		0x0004
-#define WCN36XX_DXE_CH_STATUS_REG_ADDR_TX_L	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_LOW_OFFSET + WCN36XX_DXE_CH_STATUS_REG_ADDR)
-#define WCN36XX_DXE_CH_STATUS_REG_ADDR_TX_H	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_HIGH_OFFSET + WCN36XX_DXE_CH_STATUS_REG_ADDR)
-#define WCN36XX_DXE_CH_STATUS_REG_ADDR_RX_L	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_LOW_OFFSET + WCN36XX_DXE_CH_STATUS_REG_ADDR)
-#define WCN36XX_DXE_CH_STATUS_REG_ADDR_RX_H	(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_HIGH_OFFSET + WCN36XX_DXE_CH_STATUS_REG_ADDR)
+#define WCN36XX_DXE_CH_STATUS_REG_ADDR_TX_L	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_STATUS_REG_ADDR)
+#define WCN36XX_DXE_CH_STATUS_REG_ADDR_TX_H	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_STATUS_REG_ADDR)
+#define WCN36XX_DXE_CH_STATUS_REG_ADDR_RX_L	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_LOW_OFFSET + \
+						 WCN36XX_DXE_CH_STATUS_REG_ADDR)
+#define WCN36XX_DXE_CH_STATUS_REG_ADDR_RX_H	(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_HIGH_OFFSET + \
+						 WCN36XX_DXE_CH_STATUS_REG_ADDR)
 
 
 /* DXE default control register */
-#define WCN36XX_DXE_REG_CTL_RX_L		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_LOW_OFFSET)
-#define WCN36XX_DXE_REG_CTL_RX_H		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_RX_HIGH_OFFSET)
-#define WCN36XX_DXE_REG_CTL_TX_H		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_HIGH_OFFSET)
-#define WCN36XX_DXE_REG_CTL_TX_L		(WCN36XX_DXE_MEM_REG + WCN36XX_DXE_TX_LOW_OFFSET)
+#define WCN36XX_DXE_REG_CTL_RX_L		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_LOW_OFFSET)
+#define WCN36XX_DXE_REG_CTL_RX_H		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_RX_HIGH_OFFSET)
+#define WCN36XX_DXE_REG_CTL_TX_H		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_HIGH_OFFSET)
+#define WCN36XX_DXE_REG_CTL_TX_L		(WCN36XX_DXE_MEM_REG + \
+						 WCN36XX_DXE_TX_LOW_OFFSET)
 
 #define WCN36XX_SMSM_WLAN_TX_ENABLE		0x00000400
 #define WCN36XX_SMSM_WLAN_TX_RINGS_EMPTY	0x00000200
@@ -158,26 +191,35 @@ enum wcn36xx_dxe_ch_desc_num {
 	WCN36XX_DXE_CH_DESC_NUMB_RX_H		= 40
 };
 
+/**
+ * struct wcn36xx_dxe_desc - describes descriptor of one DXE buffer
+ *
+ * @ctrl: is a union that consists of following bits:
+ * union {
+ *	u32	valid		:1; //0 = DMA stop, 1 = DMA continue with this
+ *				    //descriptor
+ *	u32	transfer_type	:2; //0 = Host to Host space
+ *	u32	eop		:1; //End of Packet
+ *	u32	bd_handling	:1; //if transferType = Host to BMU, then 0
+ *				    // means first 128 bytes contain BD, and 1
+ *				    // means create new empty BD
+ *	u32	siq		:1; // SIQ
+ *	u32	diq		:1; // DIQ
+ *	u32	pdu_rel		:1; //0 = don't release BD and PDUs when done,
+ *				    // 1 = release them
+ *	u32	bthld_sel	:4; //BMU Threshold Select
+ *	u32	prio		:3; //Specifies the priority level to use for
+ *				    // the transfer
+ *	u32	stop_channel	:1; //1 = DMA stops processing further, channel
+ *				    //requires re-enabling after this
+ *	u32	intr		:1; //Interrupt on Descriptor Done
+ *	u32	rsvd		:1; //reserved
+ *	u32	size		:14;//14 bits used - ignored for BMU transfers,
+ *				    //only used for host to host transfers?
+ * } ctrl;
+ */
 struct wcn36xx_dxe_desc {
 	u32	ctrl;
-/*
-	//TODO: Drop these, the hard coded values above and start using hal.h
-	union {
-		u32	valid		:1;     //0 = DMA stop, 1 = DMA continue with this descriptor
-		u32	transfer_type	:2;     //0 = Host to Host space
-		u32	eop		:1;     //End of Packet
-		u32	bd_handling	:1;          //if transferType = Host to BMU, then 0 means first 128 bytes contain BD, and 1 means create new empty BD
-		u32	siq		:1;     // SIQ
-		u32	diq		:1;     // DIQ
-		u32	pdu_rel		:1;     //0 = don't release BD and PDUs when done, 1 = release them
-		u32	bthld_sel	:4;     //BMU Threshold Select
-		u32	prio		:3;     //Specifies the priority level to use for the transfer
-		u32	stop_channel	:1;     //1 = DMA stops processing further, channel requires re-enabling after this
-		u32	intr		:1;     //Interrupt on Descriptor Done
-		u32	rsvd		:1;     //reserved
-		u32	size		:14;    //14 bits used - ignored for BMU transfers, only used for host to host transfers?
-	} desc_ctl;
-*/
 	u32	fr_len;
 
 	u32	src_addr_l;
