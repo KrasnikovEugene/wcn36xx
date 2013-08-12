d766f0f4c17b5ffbd1d4092fe01ba2a6f9b4749a
Eugene Krasnikov <k.eugene.e@gmail.com>
2013-08-12 17:55:19 +0200

    In AP mode delete bss when BEACON is disabled
    Delete bss when AP mode is stopped to put HW in initial state.
    
    Signed-off-by: Eugene Krasnikov <k.eugene.e@gmail.com>

--- main.c
+++ main.c
@@ -618,7 +618,9 @@ static void wcn36xx_bss_info_changed(struct ieee80211_hw *hw,
 			wcn36xx_smd_set_link_st(wcn, vif->addr, vif->addr,
 						link_state);
 		} else {
-			/* FIXME: disable beaconing */
+			wcn36xx_smd_set_link_st(wcn, vif->addr, vif->addr,
+						WCN36XX_HAL_LINK_IDLE_STATE);
+			wcn36xx_smd_delete_bss(wcn);
 		}
 	}
 out:
